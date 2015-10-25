d827c0be20a89577d71bf28d09743eb923b106d7
Khoa Nguyen <khoa02031995@gmail.com>
2015-10-26 02:08:39 +0700

    Added sitemap and footer

--- wp-content/themes/minimal-master/js/masonry-init.js
+++ wp-content/themes/minimal-master/js/masonry-init.js
@@ -1,9 +1,1 @@
-var container = document.querySelector('.content');
-var msnry;
-// initialize Masonry after all images have loaded
-imagesLoaded( container, function() {
-  msnry = new Masonry( container, {
-  	itemSelector: '.entry',
-  	gutter: 40
-  });
-});
+var container=document.querySelector('.content');var msnry;imagesLoaded(container,function(){msnry=new Masonry(container,{itemSelector:'.entry',gutter:40});});
